2464cac60ceb8d82fc4d6ba8597daea870be6256
Adithyan A <deepaadithyan56@gmail.com>
2024-04-22 02:06:43 +0530

    Search Page Created

--- components/search-input.jsx
+++ components/search-input.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
-import { Image, TextInput, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
+import { Alert, Image, TextInput, TouchableOpacity, View } from "react-native";
 import { icons } from "../constants";
+import { usePathname } from "expo-router";
 
-export default function SearchInput({ title, value, onChangeText, otherClassNames, ...props }) {
-  const [showPassword, setShowPassword] = useState(false);
+export default function SearchInput({ initialQuery }) {
+  const { pathname } = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
 
   return (
-    <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
+    <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
       <TextInput
-        className="mt-0.5 flex-1 text-white font-pregular text-base"
-        value={value}
-        placeholder="Search for a video topic"
-        placeholderTextColor="#7b7b8b"
-        onChangeText={onChangeText}
-        secureTextEntry={title === "Password" && !showPassword}
-        {...props}
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={query}
+        placeholder="Search a video topic"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
       />
 
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) return Alert.alert("Missing Query", "Please enter a search query");
+          if (pathname === `/search/`) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
         <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
       </TouchableOpacity>
     </View>
